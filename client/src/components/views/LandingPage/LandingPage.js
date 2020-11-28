d8b1491cb8db247943d919f9d410101a0b18ec22
jhyounyaho <https://github.com/jhyounyaho>
2020-11-28 20:39:29 +0900

    로그아웃 기능 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,31 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import { PromiseProvider } from 'mongoose';
 
-function LandingPage() {
+function LandingPage(props) {
   useEffect(() => {
     // server request, port: server 5000, client 3000 
     axios.get('/api/hello')
       .then(response => console.log(response.data))
   }, []) 
 
+  const onClickHandler = () => {
+    axios.get('/api/users/logout')
+      .then(response => {
+        if(response.data.success) {
+          props.history.push('/login')
+        } else {
+          alert('logout fail!')
+        }
+      })
+  }
+
   return (
     <div style={{ display:'flex', justifyContent:'center', alignItems:'center', width:'100%', height:'100vh' }}>
       <h2>시작 페이지</h2>
+
+      <br />
+      <button onClick={onClickHandler}>log out</button>
     </div>
   )
 }
