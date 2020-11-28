ea32699531a22dffed4b366ceb2c67bd9db7c6e2
jhyounyaho <https://github.com/jhyounyaho>
2020-11-28 17:37:39 +0900

    redux를 사용한 로그인 페이지 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -9,8 +9,8 @@ function LandingPage() {
   }, []) 
 
   return (
-    <div>
-      LandingPage 
+    <div style={{ display:'flex', justifyContent:'center', alignItems:'center', width:'100%', height:'100vh' }}>
+      <h2>시작 페이지</h2>
     </div>
   )
 }
--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_actions/user_actions';
 
-function LoginPage() {
+function LoginPage(props) {
+  const dispatch = useDispatch();
+
+  // useState 를 사용한 상태관리 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // email handler
+  const onEmailHandler = (e) => {
+    setEmail(e.currentTarget.value);
+  }
+
+  // password handler
+  const onPasswordHandler = (e) => {
+    setPassword(e.currentTarget.value);
+  }
+
+  // form submit handler 
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    // 유효성 검사 필요시 여기서 작성 ex) 비밀번호 길이, 조합 등등  
+
+    let body = {
+      email,
+      password
+    }
+
+    dispatch(loginUser(body))
+    .then(response => {
+      if (response.payload.loginSuccess) {
+        // 로그인 성공시 LandingPage 이동 
+        props.history.push('/')
+      } else {
+        alert('Error˝')
+      }
+    })
+  }
+
   return (
-    <div>
-      LoginPage  
+    <div style={{ display:'flex', justifyContent:'center', alignItems:'center', width:'100%', height:'100vh' }}>
+      <form 
+        style={{ display:'flex', flexDirection:'column' }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type='email' value={email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type='password' value={password} onChange={onPasswordHandler} />
+        <br />
+        <button type='submit'>Login</button>
+      </form>
     </div>
   )
 }
